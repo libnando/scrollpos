b8968f53c650c8562c7845d9bf17c94f93059139
Fernando Rosa <nando.megaman@gmail.com>
2019-03-09 17:52:49 -0300

    .

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
 const debounce = (fn) => {
 
     let frame;
-
+  
     return (...params) => {
-
-        if (frame) {
-            cancelAnimationFrame(frame);
-        }
-
-        frame = requestAnimationFrame(() => {
-
-            fn(...params);
-
-        });
-
-    }
-};
-
-const storeScroll = () => {
-    document.addEventListener('scroll', debounce(storeScroll), {
-        passive: true
-    });
-}
-
-storeScroll();
+      
+      if (frame) { 
+        cancelAnimationFrame(frame);
+      }
+  
+      frame = requestAnimationFrame(() => {        
+        fn(...params);
+      });
+  
+    } 
+  };
+    
+  const storeScroll = () => {
+    document.documentElement.dataset.scroll = window.scrollY;
+  }
+  
+  document.addEventListener('scroll', debounce(storeScroll), { passive: true });
+  
+  storeScroll();
